8112cd004c5f9b9bef8c7762f18a61ec452be1ea
sudhasrinivas pallam <pallam.sudhasrinivas@gmail.com>
2021-06-07 00:20:46 +0530

    Spring Boot Monolitic Application

---
+++ src/main/java/com/ecart/bookcart/BookCartApplication.java
@@ -0,0 +1,13 @@
+package com.ecart.bookcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookCartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ecart/bookcart/BookCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecart.bookcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
